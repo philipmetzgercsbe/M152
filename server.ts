6aff5feeca386ab61fb4140b8137ba10cde3d6e5
philipmetzgercsbe <philip.metzger@csbe.ch>
2019-03-26 11:07:22 +0100

    added stuff

--- server.ts
+++ server.ts
@@ -10,33 +10,36 @@ app.use('/static/large', express.static('changed/large'));
 app.use('/static/orig', express.static('img/'));
 
 app.get('/home', function (req: express.Request, res: express.Response) {
-    res.sendFile( __dirname + "/index.html");
+    res.sendFile(__dirname + "/index.html");
 });
 
+app.post('/upload', multer.single('img'), function (req, res) {
+    gm('img')
+        .write('./img/' + 'img', function (err) {
+            if (!err) console.log('done');
+        });
+    gm('img')
+        .resize(720)
+        .write('./changed/small/small_img', function (err) {
+            if (!err) console.log('done');
+        });
 
-app.post('/upload',multer.upload.single('img'), function (req, res) {
-gm('img')
-.resize(720)
-.write('./changed/small/small_img', function (err) {
-  if (!err) console.log('done');
-});
- 
-gm('img')
-.resize(1280)
-.write('./changed/medium/medium_img', function (err) {
-  if (!err) console.log('done');
-});
+    gm('img')
+        .resize(1280)
+        .write('./changed/medium/medium_img', function (err) {
+            if (!err) console.log('done');
+        });
 
-gm('img')
-.resize(1920)
-.write('./changed/large/large_img', function (err) {
-  if (!err) console.log('done');
-});
-    
+    gm('img')
+        .resize(1920)
+        .write('./changed/large/large_img', function (err) {
+            if (!err) console.log('done');
+        });
+
 });
 
 app.get('/', function (req, res) {
-    res.sendFile(__dirname +"index.html");
+    res.sendFile(__dirname + "index.html");
 });
 
 app.listen(80, function () {
