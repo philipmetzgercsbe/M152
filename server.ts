3c4f26a1a0ed5963564b21abd117c5869190292e
philipmetzgercsbe <philip.metzger@csbe.ch>
2019-03-26 10:37:28 +0100

    heroku initial

---
+++ server.ts
@@ -0,0 +1,44 @@
+import * as express from "express";
+import * as gm from "gm";
+import * as multer from "multer";
+
+const app = express();
+
+app.use('/static/small', express.static('changed/small'));
+app.use('/static/medium', express.static('changed/medium'));
+app.use('/static/large', express.static('changed/large'));
+
+app.get('/home', function (req: express.Request, res: express.Response) {
+    res.sendFile( __dirname + "/index.html");
+});
+
+
+app.post('/',multer.upload.single('img'), function (req, res) {
+
+gm('img')
+.resize(720)
+.write('./changed/small/small_img', function (err) {
+  if (!err) console.log('done');
+});
+ 
+gm('./img/schmitz_v3.png')
+.resize(1280)
+.write('./changed/medium/medium_img', function (err) {
+  if (!err) console.log('done');
+});
+
+gm('img')
+.resize(1920)
+.write('./changed/large/large_img', function (err) {
+  if (!err) console.log('done');
+});
+    
+});
+
+app.get('/', function (req, res) {
+    res.send("index.html");
+});
+
+app.listen(80, function () {
+    console.log("Server listens on port " + 80);
+});
